82ef534123667828407da0a7acde25307f270623
Dima Ani <dimaanisimov5665@mail.com>
2024-07-10 16:41:35 +0700

    взялся за первую главу

---
+++ немного теории/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Глава 1.</title>
+</head>
+<body>
+    <h2>Примеры с пояснениями автора</h2>
+    <p>Обсуждаем использование програмного обеспечения</p>
+    <p>Так-же анализируем принципы создания программ на PHP</p>
+</body>
+</html>
