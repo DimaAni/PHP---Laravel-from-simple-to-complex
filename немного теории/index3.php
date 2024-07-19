8faf849e265595a082c90bf28c729a04a5fab1d1
Dima Ani <dimaanisimov5665@mail.com>
2024-07-19 12:23:09 +0700

    приступил к 3 главе

---
+++ немного теории/index3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Глава 3.</title>
+</head>
+<body>
+    <h1>Управляющие инструкции</h1>
+    <p>
+    Управляющие инструкции в — это конструкции, позволяющие управлять
+    потоком выполнения программы. Они включают в себя условные операторы
+    (if, else, elseif, switch), циклы (for, while, do…while, foreach)
+    и обработку исключений (try…catch). Они помогают создавать логику
+    приложения, проверять условия, повторять действия и обрабатывать ошибки.
+    </p>
+</body>
+</html>
