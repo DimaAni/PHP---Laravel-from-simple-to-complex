870cfa8ba596be2754253f5c5194117dbc84a4d7
Dima Ani <dimaanisimov5665@mail.com>
2024-08-16 17:44:53 +0700

    Глава6

---
+++ немного теории/index6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        background-color: wheat;
+    }
+
+    pre {
+        color: red;
+    }   
+    </style>
+    <title>Глава. 6</title>
+</head>
+<body>
+    <h1>Полезные приемы и операции</h1>
+    <p>Обсудим то что не вошло в предущие главы. Но все же явдяются нужными и полезными.</p>
+    <h2>Ссылки</h2>
+</body>
+</html>
