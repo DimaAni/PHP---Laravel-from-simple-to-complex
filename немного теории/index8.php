e6b0a98700507328c9221294faa9273a36b1dd13
Dima Ani <dimaanisimov5665@mail.com>
2024-09-10 15:04:01 +0700

    Начал главу 8 Наследование

---
+++ немного теории/index8.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        background-color: wheat;
+        color: black;
+    }
+
+    pre {
+        color: red;
+    }
+
+    .ul pre {
+        font-size: 18px;
+        color: black;
+    }
+    
+    </style>
+    <title>Глава. 8</title>
+</head>
+<body>
+    <h1>Наследование.</h1>
+    <h2>Создание дочернего класса.</h2>
+<p>
+    
+</p>
+
+
+<pre>
+&lt;?php
+
+?&gt;
+</pre>
+<hr>
+<br>
+</body>
+</html>
