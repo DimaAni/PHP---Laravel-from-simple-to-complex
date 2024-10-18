d5388855e939ad2bbdc9900992bf55fce72f7b1e
Dima Ani <dimaanisimov5665@mail.com>
2024-10-18 10:45:10 +0700

    Расширенные механизмы ООП.

---
+++ index9.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        background-color: wheat;
+        color: black;
+    }
+
+    pre {
+        color: red;
+    }
+
+    .ul pre {
+        font-size: 18px;
+        color: black;
+    }
+    
+    </style>
+    <title>Глава. 9</title>
+</head>
+<body>
+    <h1>Расширенные механизмы ООП.</h1>
+    <h2>Абстрактные классы.</h2>
+<p>
+    
+</p>
+
+<pre>
+&lt;?php
+
+?&gt;
+</pre>
+
+<hr>
+<br>
+
+
+</body>
+</html>
