087935f8118a9bb80b273db7b00c1fce167d87de
Dima Ani <dimaanisimov5665@mail.com>
2024-10-27 09:53:17 +0700

    начало10 главыы

---
+++ index10.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        background-color: wheat;
+        color: black;
+    }
+
+    pre {
+        color: red;
+    }
+
+    .ul pre {
+        font-size: 18px;
+        color: black;
+    }
+    
+    </style>
+    <title>Глава. 10</title>
+</head>
+<body>
+  
+<pre>
+&lt;?php
+
+?&gt;
+</pre>
+
+</body>
+</html>
