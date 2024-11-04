72e4f24e9e2afd0493ce5cf1a77e66190d544be1
Dima Ani <dimaanisimov5665@mail.com>
2024-11-04 20:45:18 +0700

    Начало12 главы.Обработка парметров запароса

---
+++ index12.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    body {
+        background-color: wheat;
+        color: black;
+    }
+
+    pre {
+        color: red;
+    }
+
+    .ul pre {
+        font-size: 18px;
+        color: black;
+    }
+    
+    </style>
+    <title>Глава. 12</title>
+</head>
+<body>
+<h1>Использование PHP</h1>
+<h2>Обработка параметров запроса.</h2>
+<p>
+ 
+</p>
+
+    <pre>
+&lt;?php
+
+?&gt;
+</pre>
+<hr>
+<br>
+
+</body>
+</html>
